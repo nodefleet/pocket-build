025ab80dd0b36544168b5e13c517d9b4317cb6e8
Steven94 <albertstevenmf@hotmail.com>
2023-05-04 17:19:04 -0400

    aqt01 credentials changed for nodefleet's one

--- build.sh
+++ build.sh
@@ -9,17 +9,17 @@ export REPO="https://github.com/pokt-network/pocket-core.git"
 echo "----- Wiping all cache and image data -----"
 yes | sudo docker system prune
 yes | sudo docker image prune -a
-sudo docker rm aqt01/$DOCKER_REPO:$IMAGE_NAME 
-sudo docker rm aqt01/$POCKER_REPO_:$IMAGE_NAME 
+sudo docker rm nodefleet/$DOCKER_REPO:$IMAGE_NAME
+sudo docker rm nodefleet/$DOCKER_REPO_:$IMAGE_NAME
 yes | sudo docker system prune
 yes | sudo docker container prune
 yes | sudo docker volume prune
 
 echo "----- Buliding pocket image -----"
 cd ./docker
-sudo docker build --no-cache -t aqt01/$DOCKER_REPO:$IMAGE_NAME --build-arg BRANCH_NAME=$BRANCH_NAME --build-arg REPO=$REPO --build-arg COMMIT=$COMMIT .
-sudo docker push aqt01/$DOCKER_REPO:$BRANCH_NAME
+sudo docker build --no-cache -t nodefleet/$DOCKER_REPO:$IMAGE_NAME --build-arg BRANCH_NAME=$BRANCH_NAME --build-arg REPO=$REPO --build-arg COMMIT=$COMMIT .
+sudo docker push nodefleet/$DOCKER_REPO:$BRANCH_NAME
 cd ../docker-base
-sudo docker build  --no-cache -t aqt01/$DOCKER_REPO_:$IMAGE_NAME --build-arg BRANCH_NAME=$BRANCH_NAME --build-arg REPO=$REPO --build-arg DOCKER_REPO=$DOCKER_REPO --build-arg COMMIT=$COMMIT .
-sudo docker push aqt01/$DOCKER_REPO_:$IMAGE_NAME
+sudo docker build  --no-cache -t nodefleet/$DOCKER_REPO_:$IMAGE_NAME --build-arg BRANCH_NAME=$BRANCH_NAME --build-arg REPO=$REPO --build-arg DOCKER_REPO=$DOCKER_REPO --build-arg COMMIT=$COMMIT .
+sudo docker push nodefleet/$DOCKER_REPO_:$IMAGE_NAME
 
